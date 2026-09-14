ba0bf350b377ae21ec99fc2161875de4ac1a6505
Janel <jcamposg15@unc.edu.pe>
2026-09-13 19:08:22 -0500

    mejoras

--- views/layout.php
+++ views/layout.php
@@ -1096,6 +1096,10 @@ if (btnActivarNotificaciones) {
                                     '<?= e(app_url('solicitudes-cobro')) ?>'
                                     + '?solicitud='
                                     + encodeURIComponent(notification.solicitud_cobro_id);
+                            }else {
+                                window.location.href =
+                                    '<?= e(app_url('prestamos/ver/')) ?>'
+                                    + notification.prestamo_id;
                             }
 
                         } catch (error) {
